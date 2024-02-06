800e59a5349e100157967bf445a82140484a1a23
savcukova <olinkasavcuk@gmail.com>
2024-02-06 20:19:42 +0100

    start

---
+++ convertor.py
@@ -0,0 +1,19 @@
+def appartment_translation():
+    abs_path = ""
+    
+    
+    translation_pattern = {
+        "byt0001": "1+1",
+        "byt0002": "2+1",
+        "byt0003": "2+kk",
+        "byt0004": "3+1",
+        "byt0005": "3+kk",
+        "byt0006": "4+1",
+        "byt0007": "4+kk",
+    }
+    
+    
+
+if __name__ == "__main__":
+    appartment_translation()
+
