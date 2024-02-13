78da45984ab125197f375c3df7e870f8717f99d2
Olina Savcukova <olinkasavcuk@gmail.com>
2024-02-13 14:11:22 +0100

    main function

--- convertor.py
+++ convertor.py
@@ -1,31 +1,9 @@
-import os
+def start_translation(abs_path, translation_pattern):
+    print("zaciname")
 
 
-def start_translation(file, translation_pattern):
-    if (os.path.isfile(file)):
-        print("file exists")
-        appartment_data = read_text_file(file)
-    else:
-        print("file not found")
-        quit()
-        
-        
-def iterate_through_all_data(file):
-    results = []
-    
-    for row in file:
-        pass
-        
-        
-def read_text_file(file):
-    with open("file", mode = "r", encoding= "UTF-8") as txt:
-        return txt.readlines()
-
-
-def appartment_translation():
-    abs_path = f"{os.getcwd()}{os.sep}solution{os.sep}vstupni_data.txt"
-    
-    
+def apartment_translation():
+    abs_path = ""
     translation_pattern = {
         "byt0001": "1+1",
         "byt0002": "2+1",
@@ -40,5 +18,4 @@ def appartment_translation():
 
 
 if __name__ == "__main__":
-    appartment_translation()
-
+    apartment_translation()
