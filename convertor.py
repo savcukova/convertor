dfcbb966d6d0802a53efb39b769a3787c21a7b36
Olina Savcukova <olinkasavcuk@gmail.com>
2024-02-13 14:45:03 +0100

    done

--- convertor.py
+++ convertor.py
@@ -1,12 +1,13 @@
 import os
+from pprint import pprint
 
 
 def start_translation(file, translation_pattern):
     if os.path.isfile(file):
         print("soubor existuje")
         appartment_data = read_text_file(file)
-        new_data = iterate_through_all_data(appartment_data)
-        print(new_data)
+        new_data = iterate_through_all_data(appartment_data, translation_pattern)
+        pprint(new_data)
     else:
         print("fiel not found.")
         quit()
@@ -17,13 +18,20 @@ def read_text_file(file):
         return txt.readlines()
 
 
-def iterate_through_all_data(data):
+def iterate_through_all_data(data, translation_pattern):
     results = []
     
     for row in data:
-        results.append(row)
+        results.append(rewrite_new_appartment_type(row, translation_pattern))
         
     return results
+
+
+def rewrite_new_appartment_type(data, translation_pattern):
+    disposition, *rest = data.split(",", maxsplit=1)
+    new_entry = translation_pattern.get(disposition, "unknown disposition")
+    return ", ".join([new_entry] + rest)
+
 
 
 def appartment_translation():
